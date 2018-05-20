5161703bd01e9665fdbf7fa65e8f00e7d9af4007
dui <iam.narongsak@gmail.com>
2018-05-20 12:50:55 +0700

    Navigator Post pop OK

---
+++ FunnyNumber/ThaiNumberViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ThaiNumberViewController.swift
+//  FunnyNumber
+//
+//  Created by PDcode MacbookPRO on 20/5/2561 BE.
+//  Copyright © 2561 PDcode. All rights reserved.
+//
+
+import UIKit
+
+class ThaiNumberViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
