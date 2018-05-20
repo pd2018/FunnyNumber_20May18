93f92d10bfe68daeb5a0f6f94ab06376ca6b89a9
dui <iam.narongsak@gmail.com>
2018-05-20 10:44:33 +0700

    ShowClick OK

--- FunnyNumber/ViewController.swift
+++ FunnyNumber/ViewController.swift
@@ -9,6 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+//    เริ่มตรงนี้
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    
+    @IBAction func decBtn(_ sender: UIBarButtonItem) {
+        showClick(nameButtom: "decBtn")
+        
+    }
+    
+    
+    @IBAction func startOneBtn(_ sender: UIBarButtonItem) {
+        showClick(nameButtom: "startOne")
+    }
+    
+    
+    @IBAction func incBtn(_ sender: UIBarButtonItem) {
+        showClick(nameButtom: "incBtn")
+    }
+    
+    
+    func showClick(nameButtom: String) -> Void {
+        print("You Click ==> \(nameButtom)")
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
